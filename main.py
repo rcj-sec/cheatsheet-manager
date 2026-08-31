f7a0d1b1c9041a222566b90774ee7555854f8ccd
rcj <rcj-dev@proton.me>
2026-08-31 12:10:28 +0200

    feat: changed script options and support for common sheets directory

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 from prompt_toolkit import prompt
 
@@ -13,23 +14,29 @@ def parse_args():
     parser = argparse.ArgumentParser(prog="sheets", description="Cheatsheet manager")
 
     parser.add_argument(
-        "file", nargs="?", default="cheatsheet.json", help="Cheatsheet JSON file"
+        "sheet", nargs="?", help="Name of JSON file in default JSON dir. No extension."
     )
+    parser.add_argument("-f", "--filename")
 
     parser.add_argument(
         "-i", "--interactive", action="store_true", help="Run in interactive mode."
     )
 
-    return parser.parse_args()
+    args = parser.parse_args()
+
+    if (args.sheet is None) == (args.filename is None):
+        parser.error("Either a sheet or a file with -f must be specified")
+
+    return args
 
 
-def run_interactive_mode(args):
-    shell = InteractiveCLI(args.file)
+def run_interactive_mode(filepath):
+    shell = InteractiveCLI(filepath)
     shell.run()
 
 
-def run_gui_mode(args):
-    model = CheatSheet(args.file)
+def run_gui_mode(filepath):
+    model = CheatSheet(filepath)
     search_view = SearchView()
     edit_view = EditView()
     controller = MainControllerV2(
@@ -42,10 +49,19 @@ def run_gui_mode(args):
 def main():
     args = parse_args()
 
+    SHEETS_DIR = "$HOME/dotfiles/cheatsheets/"
+
+    sheet_path = SHEETS_DIR
+
+    if args.sheet:
+        sheet_path = os.path.expandvars(sheet_path + args.sheet + ".json")
+    if args.filename:
+        sheet_path = os.path.realpath(args.filename)
+
     if args.interactive:
-        run_interactive_mode(args)
+        run_interactive_mode(sheet_path)
     else:
-        run_gui_mode(args)
+        run_gui_mode(sheet_path)
 
 
 if __name__ == "__main__":
